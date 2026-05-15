1d880c29497a1cbc185dbd1de7ab5a7baf9ff813
Rafaela Rodrigues <107862830+luna-rafa@users.noreply.github.com>
2026-05-15 15:08:35 -0300

    Simplify rendering by removing BrowserRouter and Routes
    Removed routing setup and directly rendered the App component.

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom'
-
 import './index.css'
 import App from './App.jsx'
-import Apresentacao from './Apresentacao.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/apresentacao" element={<Apresentacao />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
